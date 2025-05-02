c558986b0725aa2b632cacc7914a559a592791ff
Yamoon224 <Yamooon664@gmail.com>
2025-05-02 12:54:53 +0000

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
     output: 'export', // Assurez-vous que la sortie est configurée pour un export statique
-    distDir: 'build', // Vous pouvez définir un répertoire personnalisé si nécessaire
+    distDir: 'dist', // Vous pouvez définir un répertoire personnalisé si nécessaire
 };
   
